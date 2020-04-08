5760d120a37ee4f903f5199bdf815b19f5c8b70a
Longtao Wang <longtao.wang@cortexlabs.ai>
2020-04-08 02:49:22 +0000

    update fix git

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 
-COM=$1
+COM="$1"
 
 fix_git() {
+  INAME=$(whoami)
   cd ~/.vim/bundle/YouCompleteMe
-  sed -i 's/\/serving\//\/wlt\//g' `grep "/serving/" -rl YouCompleteMe`
+  sed -i "s/\/serving\//\/${INAME}\//g" `grep "/serving/" -rl YouCompleteMe`
   find . -name '*.pyc' -delete
   cd -
 }
+
+if [[ $COM == "--fix-git" ]]; then
+  fix_git
+fi
