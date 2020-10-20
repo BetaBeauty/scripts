86e2bbee403d982a50ebef6c72f66144b37fec39
Longtao Wang <longtao.wang@cortexlabs.ai>
2020-10-20 18:13:52 +0800

    update n2n

--- n2n/start.sh
+++ n2n/start.sh
@@ -21,6 +21,28 @@ do_start_edgenode() {
   edge conf.edge
 }
 
+do_dhcp_setup() {
+  # On supernode
+  dnf install dhcp-server dhcp-client n2n -y
+  systemctl disable dhcpd --now
+  supernode -f -l 9090 -v
+
+  # On supernode/one edge
+  edge -f -d edge0 -r -a 192.168.1.0 -c foo -k bar -l localhost:9090 -v -A
+  cat > /etc/dhcp/dhcpd.conf <<EOF
+  subnet 192.168.1.0 netmask 255.255.255.0 {
+          range 192.168.1.10 192.168.1.100;
+  }
+EOF
+  dhcpd -f edge0
+
+  # On all other edges
+  dnf install dhcp-client n2n -y
+  edge -f -d edge0 -r -a dhcp:0.0.0.0 -c foo -k bar -l 116.203.50.42:9090 -v -A -m $(echo $(hostname)|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/') # To get the same MAC address and thus the same IP address every time
+  pkill -9 dhclient; dhclient edge0
+
+}
+
 case "$1" in
   install)
   do_install
