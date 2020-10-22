c1d23306f8e77ceef70de44462b32c5895657949
Longtao Wang <longtao.wang@cortexlabs.ai>
2020-10-22 15:43:09 +0800

    update n2n config

--- n2n/edge.sh
+++ n2n/edge.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-[ -z "$N2N_PATH" ] && N2N_PATH=/usr/sbin
-
 DESC='n2n P2P VPN'
 NAME=n2n
+
+[ ! -r ./n2n.conf ] && cp ./etc/default/$NAME ./n2n.conf
+. ./n2n.conf
+
 DAEMON=$N2N_PATH/edge
 DAEMON_ARGS=""             # Arguments to run the daemon with
 
@@ -13,16 +15,17 @@ if [ ! -x "$DAEMON" ]; then
   exit 1
 fi
 
-[ -r ./etc/default/$NAME ] && . ./etc/default/$NAME
-
-if [ -z "$N2N_EDGE_CONFIG_DONE" ]; then
-	echo "Warning: n2n VPN client is not configured, edit config file in etc/default/$NAME." 1>&2
+if [ -z "$N2N_CONFIG_DONE" ]; then
+	echo "Warning: n2n VPN client is not configured, edit config file in n2n.conf." 1>&2
 	exit 0
 fi
 
 [ -z "$N2N_IP" ] || DAEMON_ARGS="-a $N2N_IP $DAEMON_ARGS"
 [ -z "$N2N_MANA_PORT" ] || DAEMON_ARGS="-t $N2N_MANA_PORT $DAEMON_ARGS"
+DAEMON_ARGS="-c $N2N_COMMUNITY -l $N2N_SUPERNODE -u $(id -u nobody) \
+             -g $(id -u nobody) -f $N2N_VERBOSITY $DAEMON_ARGS"
 
 export N2N_KEY
-$DAEMON -c $N2N_COMMUNITY -l $N2N_SUPERNODE -u $(id -u nobody) -g $(id -g nobody) \
-  -f $DAEMON_ARGS || return 2
+EXEC_COM="$DAEMON $DAEMON_ARGS"
+echo "Exec: $EXEC_COM"
+$EXEC_COM || exit 2
---
+++ n2n/super.sh
@@ -0,0 +1,21 @@
+
+DESC='n2n P2P VPN'
+NAME=n2n
+
+[ ! -r ./n2n.conf ] && cp ./etc/default/$NAME ./n2n.conf
+. ./n2n.conf
+
+DAEMON=$N2N_PATH/supernode
+DAEMON_ARGS=""             # Arguments to run the daemon with
+
+if [ -z "$N2N_CONFIG_DONE" ]; then
+	echo "Warning: n2n VPN super node is not configured, edit config file in n2n.conf." 1>&2
+	exit 0
+fi
+
+DAEMON_ARGS="$N2N_VERBOSITY $DAEMON_ARGS"
+[ ! -x "$N2N_IP_POOLS" ] && DAEMON_ARGS="-a $N2N_IP_POOLS $DAEMON_ARGS"
+
+EXEC_COM="$DAEMON $DAEMON_ARGS"
+echo "Exec: $EXEC_COM"
+$EXEC_COM || exit 2
