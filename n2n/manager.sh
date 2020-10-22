ce6387844a351f15e9fa9f71cb7f414209c9da87
Longtao Wang <longtao.wang@cortexlabs.ai>
2020-10-22 16:48:40 +0800

    update manager

--- n2n/manager.sh
+++ n2n/manager.sh
@@ -1,9 +1,19 @@
 
-[ -z "$N2N_CONF_PATH" ] && N2N_CONF_PATH=./etc/default/n2n
+if [ -z "./n2n.conf" ]; then
+  . ./n2n.conf
+elif [ -z "/etc/default/n2n" ]; then
+  . /etc/default/n2n
+fi
 
-[ -r $N2N_CONF_PATH ] && . $N2N_CONF_PATH
 
-[ -z "$N2N_MANA_PORT" ] && N2N_MANA_PORT=5644
+case $1 in
+  -s|--super)
+  [ -z "$N2N_MANA_PORT" ] && N2N_MANA_PORT=5645
+  ;;
+  *)
+  [ -z "$N2N_MANA_PORT" ] && N2N_MANA_PORT=5644
+  ;;
+esac
 
 echo "press Enter for static information query:"
 netcat -u localhost $N2N_MANA_PORT
