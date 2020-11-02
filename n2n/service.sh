d38b51e6922aafc3fb32a1cbcf7b76ddecfbfcdf
Longtao Wang <longtao.wang@cortexlabs.ai>
2020-11-02 17:16:11 +0800

    update

--- n2n/service.sh
+++ n2n/service.sh
@@ -19,6 +19,7 @@ do_install() {
   if [ ! -f "/etc/init.d/n2n" ]; then
     sudo cp -r etc /
     sudo systemctl daemon-reload
+    sudo update-rc.d n2n defaults
   fi
 
   echo "Please edit the configuration file: /etc/default/n2n for your edge node"
